3181622cc1d0bd178b616f6bae8281e82f1e7eca
futayama <futa.ymkw@gmail.com>
2026-09-03 03:33:05 +0900

    feat: usersの仮実装とmigration

---
+++ apps/api/prisma/migrations/20260902183158_create_users/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
